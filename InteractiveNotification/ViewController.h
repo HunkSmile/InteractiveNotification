2510c249f7b182673eb327edd88ed163047ab347
Hunk <lixuwensmile@gmail.com>
2014-10-24 14:01:07 +0800

    Init interactive notification

---
+++ InteractiveNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InteractiveNotification
+//
+//  Created by Hunk on 14/10/24.
+//  Copyright (c) 2014年 Hunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InteractiveNotification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InteractiveNotification
+//
+//  Created by Hunk on 14/10/24.
+//  Copyright (c) 2014年 Hunk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
